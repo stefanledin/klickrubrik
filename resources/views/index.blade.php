46aa1385834bd867b62a41fb7c14d84495d2f53b
stefanledin <stefan.ledin@gmail.com>
2015-08-15 11:43:59 +0200

    Lagt till fler punchlines

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -22,32 +22,57 @@
                             <input type="text" name="what" id="what" class="form-control" placeholder="vad?">
                         </div>
                         <div class="form-group">
-                            <select name="and-but" id="and-but" class="form-control">
-                                <option value="och">och</option>
-                                <option value="men">men</option>
+                            <select name="punchline" id="punchline" class="form-control">
+                                <option value="">och du kan inte gissa vad som hände sen!</option>
+                                <option value="">– du anar inte vad som vad hände!</option>
+                                <option value="">och det som hände sen har fått en hel värld att förundras!</option>
+                                <option value="">och det som hände sen har fått ett helt land att förundras!</option>
+                                <option value="">och det som hände sen har fått en hel läkarkår att förundras!</option>
+                                <option value="">det som hände sen kommer förändra din syn på mänskligheten!</option>
+                                <option value="">och det som sen hände fick mig att gråta!</option>
+                                <option value="">– resultatet är chockerande!</option>
+                                <option value="">och resultatet har chockat en hel värld!</option>
+                                <option value="">– ingen kunde ana följderna!</option>
+                                <option value="">– konsekvenserna var oanade!</option>
+                                <option value="">– ingen kunde förutse följderna!</option>
                             </select>
                         </div>
-                        <h2>du kan inte gissa vad som hände sen!</h2>
                         <hr>
-                        <div class="well well-sm">
+                        <div class="well well-lg">
                             <h3>Vad hände sen?</h3>
-                            <div class="row">
-                                <div class="col-xs-12 col-sm-6">
-                                    <div class="form-group">
-                                        <label for="upload-image">Ladda upp bild</label>
-                                        <input type="file" name="upload-image" id="upload-image" class="form-control">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">Bild</h3>
+                                </div>
+                                <div class="panel-body">
+                                    <div class="row">
+                                        <div class="col-xs-12 col-sm-6">
+                                            <div class="form-group">
+                                                <label for="upload-image">Ladda upp bild</label>
+                                                <input type="file" name="upload-image" id="upload-image" class="form-control">
+                                            </div>
+                                        </div>
+                                        <div class="col-xs-12 col-sm-6">
+                                            <div class="form-group">
+                                                <label for="image-link">Länk till bild</label>
+                                                <input type="text" name="image-link" id="image-link" class="form-control">
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="col-xs-12 col-sm-6">
-                                    <div class="form-group">
-                                        <label for="image-link">Länk till bild</label>
-                                        <input type="text" name="image-link" id="image-link" class="form-control">
-                                    </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">Film</h3>
                                 </div>
-                                <div class="col-xs-12">
-                                    <div class="form-group">
-                                        <label for="youtube-link">YouTube-länk</label>
-                                        <input type="text" name="youtube-link" id="youtube-link" class="form-control">
+                                <div class="panel-body">
+                                    <div class="row">
+                                        <div class="col-xs-12">
+                                            <div class="form-group">
+                                                <label for="youtube-link">YouTube-länk</label>
+                                                <input type="text" name="youtube-link" id="youtube-link" class="form-control">
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
